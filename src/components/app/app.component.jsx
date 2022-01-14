8036bbe4d01442cae30bfd746780b8f30f42a269
Aman Gupta <amang5840@gmail.com>
2022-01-15 00:06:15 +0530

    create directory structure

---
+++ src/components/app/app.component.jsx
@@ -0,0 +1,12 @@
+import "./app.component.css";
+import { Tooltip } from "..";
+
+function App() {
+  return (
+    <div>
+      <Tooltip />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/tooltip/tooltip.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./tooltip.component.css";
+
+const Tooltip = () => {
+  return <div>tootlip</div>;
+};
+
+export default Tooltip;
